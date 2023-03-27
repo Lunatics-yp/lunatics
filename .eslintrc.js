e243d5d6deaa550359e79606c9d35b70dd820186
djpont <34692754+djpont@users.noreply.github.com>
2023-03-27 18:16:20 +0400

    Инициализация проекта
    * Тест работы гита
    
    * Настроил tsconfig и создал директории
    
    * Настроил eslint и stylelint
    
    * Настроил jest
    
    * Добавил папки
    
    * Обновил .editorconfig
    
    * Зафиксировал версии
    
    * Фиксация актуальных версий пакетов. Перевод всех на YARN.
    
    * Добавление базовых css-стилей
    
    * Перенос ".eslintignore" в параметр "ignorePatterns" (правила из файла .eslintignore не работали)
    
    ---------
    
    Co-authored-by: juliv <julivgan@gmail.com>
    Co-authored-by: Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 1,
-  },
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	"parserOptions": {
+		"ecmaVersion": 11
+	},
+	env: {
+		browser: true,
+		es2020: true,
+		node: true,
+	},
+	ignorePatterns: [
+		"**/dist/*",
+		"/node_modules",
+		"/tmp",
+		"/server.js"
+	],
+	"rules": {
+		// Правила от djpont
+		"max-len": [2, 100],
+		"max-params": [2, 3],
+		"no-unused-vars": 0,
+		"@typescript-eslint/no-unused-vars": ["off"], //error
+		"@typescript-eslint/no-inferrable-types": ["off"],
+		// Правила, которые были изначально
+		'@typescript-eslint/ban-ts-comment': 1,
+	}
 }
