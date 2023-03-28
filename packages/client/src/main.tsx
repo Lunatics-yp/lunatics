54930d262d323d0cc0fb3426cc8f2b88c19d9d62
IGrekov <4romangrekk@gmail.com>
2023-03-28 20:18:05 +0300

    Добавил роутинг и тестовую страницу(демоверсия)

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { PageExample } from './pages/Example';
+import { TestPage } from './pages/TestPage';
+import { Background } from 'client/src/components/Background';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import 'client/src/styles/root.scss';
 import 'client/src/styles/app.scss';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PageExample />,
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Background />
   </React.StrictMode>
 );
--- packages/client/src/pages/Example/index.tsx
+++ packages/client/src/pages/Example/index.tsx
@@ -1,13 +1,16 @@
-import {Header} from 'client/src/components/Header';
-import {Menu} from 'client/src/components/Menu';
-import {Switch} from 'client/src/components/Switch';
-import {Button} from 'client/src/components/Button';
-import {Input} from 'client/src/components/Input';
-import {Footer} from 'client/src/components/Footer';
+import { Header } from 'client/src/components/Header';
+import { Menu } from 'client/src/components/Menu';
+import { Switch } from 'client/src/components/Switch';
+import { Button } from 'client/src/components/Button';
+import { Input } from 'client/src/components/Input';
+import { Footer } from 'client/src/components/Footer';
 import './example.scss';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 export const PageExample = () => {
 
+	const navigate = useNavigate();
 	const buttonCallback = () => {
 		console.log('Клик');
 	};
@@ -43,9 +46,12 @@ export const PageExample = () => {
 					onClick={buttonCallback}
 				/>
 				<Button
-					text='Кнопка 3'
-					onClick={buttonCallback}
+					text='Переход на страницу test'
+					onClick={() => {
+						navigate('/test');
+					}}
 				/>
+				<Link to='/test'>Переход на страницу test</Link>
 			</Menu>
 			<Footer>Подвал</Footer>
 		</div>
