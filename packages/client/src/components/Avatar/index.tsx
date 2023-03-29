398983b9560709939faa660a7644e612ca16001f
Moriarty25 <afanasev.andrey99@gmail.com>
2023-03-29 18:12:12 +0300

    фикс ковычек и пробелов

--- packages/client/src/components/Avatar/index.tsx
+++ packages/client/src/components/Avatar/index.tsx
@@ -1,14 +1,14 @@
 import {FC} from 'react';
 import {AvatarProps} from './typing';
 import defaultUserPhoto from 'client/src/assets/images/defaultUserPhoto.jpg';
-import  styles from './Avatar.module.scss';
+import styles from './Avatar.module.scss';
 
-export const Avatar: FC<AvatarProps> = ({src, largePhoto=false}) => {
+export const Avatar: FC<AvatarProps> = ({src, largePhoto = false}) => {
 	return (
 		<div className={styles.avatar__wrapper}>
 			<img
 				src={src ?? defaultUserPhoto}
-				alt="user-photo"
+				alt='user-photo'
 				className={`${styles.avatar} ${largePhoto ? `${styles.avatar_large}` : ''} `}
 			/>
 		</div>
