dfa1deb978b415a6acaa7738ec90458e67180236
Moriarty25 <afanasev.andrey99@gmail.com>
2023-03-30 11:08:11 +0300

    вынесение класса в переменную

--- packages/client/src/components/Avatar/index.tsx
+++ packages/client/src/components/Avatar/index.tsx
@@ -3,13 +3,16 @@ import {AvatarProps} from './typing';
 import defaultUserPhoto from 'client/src/assets/images/defaultUserPhoto.jpg';
 import './Avatar.scss';
 
-export const Avatar: FC<AvatarProps> = ({src, alt, size = 'small'}) => {
+export const Avatar: FC<AvatarProps> = ({src, alt = '', size = 'small'}) => {
+
+	const className = `avatar__img avatar--${size}`;
+
 	return (
 		<div className='avatar'>
 			<img
 				src={src ?? defaultUserPhoto}
 				alt={alt}
-				className = {`${'avatar__img'} ${'avatar--'+size}`}
+				className = {className}
 			/>
 		</div>
 	);
