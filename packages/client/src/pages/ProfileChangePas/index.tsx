32d00f0c2e589e2675137d446efbade380ebdd4d
IGrekov <4romangrekk@gmail.com>
2023-03-31 22:30:05 +0300

    Добавил страницы профиля

---
+++ packages/client/src/pages/ProfileChangePas/index.tsx
@@ -0,0 +1,43 @@
+import {Form} from 'react-router-dom';
+import {Header} from 'client/src/components/Header';
+import {Button} from 'client/src/components/Button';
+import {Input} from 'client/src/components/Input';
+import {Footer} from 'client/src/components/Footer';
+import {Avatar} from 'client/src/components/Avatar';
+import './profile.scss';
+import 'client/src/styles/form.scss';
+
+export const PageProfileChangePas = () => {
+
+	const buttonCallback = () => {
+		console.log('Клик');
+	};
+	return (
+		<div className='pageProfile'>
+			<Header>Прифиль: смена пароля</Header>
+			<div>
+
+				<Form className="form">
+					<Avatar
+						size='large'
+					/>
+					<Input
+						label='Текущий пароль'
+						type='password'
+					/>
+					<Input
+						label='Новый пароль'
+						type='password'
+					/>
+					<div className="formGroup_btns">
+						<Button
+							text='Сохранить'
+							onClick={buttonCallback}
+						/>
+					</div>
+				</Form>
+			</div>
+			<Footer>Подвал</Footer>
+		</div>
+	);
+};
