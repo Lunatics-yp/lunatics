40160cd4c8f5a05802e4c519ece88adeb7a5a379
djpont <alexandervotyakov@gmail.com>
2023-04-01 11:41:58 +0400

    fix: попытка починить конфликт 2

--- packages/client/src/routers/index.tsx
+++ packages/client/src/routers/index.tsx
@@ -1,7 +1,10 @@
-
 import {createBrowserRouter} from 'react-router-dom';
 import {PageExample} from 'client/src/pages/Example';
-import {MainMenu, mainMenuLoader} from 'client/src/pages/MainMenu';
+import {MainMenu} from 'client/src/pages/MainMenu';
+import {MainMenuMain} from '../pages/MainMenu/pages/main';
+import {MainMenuPlayAgainstAI} from '../pages/MainMenu/pages/playAgainstAI';
+import {MainMenuPlayOnline} from '../pages/MainMenu/pages/playOnline';
+import {MainMenuSettings} from '../pages/MainMenu/pages/settings';
 import {TestPage} from 'client/src/pages/TestPage';
 import {PATHS} from 'client/src/routers/name';
 import {Page500} from 'client/src/pages/500';
@@ -12,36 +15,48 @@ import {PageAuth} from 'client/src/pages/Auth';
 export const router = createBrowserRouter([
 	{
 		path: PATHS.home,
-		element: <PageExample />
+		element: <PageExample/>
 	},
 	{
 		path: PATHS.test,
-		element: <TestPage />
+		element: <TestPage/>
 	},
 	{
 		path: PATHS.mainMenu,
-		element: <MainMenu />,
-		loader: mainMenuLoader(PATHS.mainMenu)
-	},
-	{
-		path: `${PATHS.mainMenu}/:subPageId`,
-		element: <MainMenu />,
-		loader: mainMenuLoader(PATHS.mainMenu)
+		element: <MainMenu/>,
+		children: [
+			{
+				path: PATHS.mainMenu,
+				element: <MainMenuMain/>
+			},
+			{
+				path: PATHS.mainMenuPlayAgainstAI,
+				element: <MainMenuPlayAgainstAI/>
+			},
+			{
+				path: PATHS.mainMenuPlayOnline,
+				element: <MainMenuPlayOnline/>
+			},
+			{
+				path: PATHS.mainMenuSettings,
+				element: <MainMenuSettings/>
+			}
+		]
 	},
 	{
 		path: PATHS.page500,
-		element: <Page500 />
+		element: <Page500/>
 	},
 	{
 		path: PATHS.page404,
-		element: <Page404 />
+		element: <Page404/>
 	},
 	{
 		path: PATHS.register,
-		element: <PageRegister />
+		element: <PageRegister/>
 	},
 	{
 		path: PATHS.auth,
-		element: <PageAuth />
+		element: <PageAuth/>
 	}
 ]);
