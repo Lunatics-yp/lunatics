591aacf8f0a9b1c328598483b649aea868617dc0
IGrekov <4romangrekk@gmail.com>
2023-04-01 10:51:50 +0300

    fix: исправление server

--- packages/client/src/components/ButtonBack/typing.ts
+++ packages/client/src/components/ButtonBack/typing.ts
@@ -1,5 +1,7 @@
-export type  AvatarProps = {
-	src?: string;
-	alt?: string;
-	size: 'small' | 'medium' | 'large';
+export type TButtonBackProps = {
+	// Текст кнопки
+	text: string;
+	//путь для перехода
+	href: string;
 };
+
