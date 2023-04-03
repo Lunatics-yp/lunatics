38539e3a2d1237af32f9db6701f4ac971c618f7e
IGrekov <4romangrekk@gmail.com>
2023-04-03 23:23:59 +0300

    ✨ Смена пароля и данных пользователя

--- packages/client/src/pages/Profile/index.tsx
+++ packages/client/src/pages/Profile/index.tsx
@@ -3,56 +3,76 @@ import {Header} from 'client/src/components/Header';
 import {Button} from 'client/src/components/Button';
 import {Input} from 'client/src/components/Input';
 import {Avatar} from 'client/src/components/Avatar';
+import {useState} from 'react';
+import profile from 'client/src/api/profile';
 import 'client/src/styles/form.scss';
 import './profile.scss';
 
 export const PageProfile = () => {
+
+	// const { login, email } = useSelector('из стора возьму данные пользователя')
+
+	const [loginEmail, setLoginEmail] = useState({
+		login: 'admin',
+		email: 'zzz@gamil'
+	});
 	const navigate = useNavigate();
 
 	const buttonCallback = () => {
-		console.log('Клик');
+		profile.changeUserForm(loginEmail);
 	};
+
+	const onAvatarChange = (file) => {
+		const formData = new FormData();
+		formData.append('avatar', file);
+		// api call change avatar
+	};
+
+	const onChangeInputName = (value, name) => {
+		setLoginEmail(_prevValues => ({..._prevValues, [name]: value}));
+	};
+
 	return (
 		<div className='pageProfile'>
 			<Header>Прифиль игрока</Header>
-			<div>
-				<Button
-					text='назад'
-					className='buttonBack'
-					onClick={() => {
-						navigate('/');
-					}}
+			<div></div>
+			<Button
+				text='назад'
+				className='buttonBack'
+				onClick={() => {
+					navigate('/');
+				}}
+			/>
+			<Form className="form">
+				<Avatar
+					size='large'
+					editable
+					onClick={onAvatarChange}
 				/>
-				<Form className="form">
-					<Avatar
-						size='large'
-						editable={true}
+				<Input
+					label='Логин'
+					name='login'
+					onChange={onChangeInputName}
+				/>
+				<Input
+					label='E-mail'
+					type='email'
+					name='email'
+					onChange={onChangeInputName}
+				/>
+				<div className='formGroup_btns'>
+					<Button
+						text='Смена пароля'
+						onClick={() => {
+							navigate('/profileChangePassword');
+						}}
 					/>
-					<Input
-						label='Логин'
+					<Button
+						text='Сохранить'
+						onClick={buttonCallback}
 					/>
-					<Input
-						label='E-mail'
-						type='email'
-					/>
-					<div className='formGroup_btns'>
-						<Button
-							text='Смена аватарки'
-							onClick={buttonCallback}
-						/>
-						<Button
-							text='Смена пароля'
-							onClick={() => {
-								navigate('/profileChangePassword');
-							}}
-						/>
-						<Button
-							text='Сохранить'
-							onClick={buttonCallback}
-						/>
-					</div>
-				</Form>
-			</div>
+				</div>
+			</Form>
 		</div>
 	);
 };
--- packages/client/src/pages/ProfileChangePassword/index.tsx
+++ packages/client/src/pages/ProfileChangePassword/index.tsx
@@ -1,17 +1,31 @@
+import {useState} from 'react';
 import {Form, useNavigate} from 'react-router-dom';
 import {Header} from 'client/src/components/Header';
 import {Button} from 'client/src/components/Button';
 import {Input} from 'client/src/components/Input';
 import {Avatar} from 'client/src/components/Avatar';
+import profile from 'client/src/api/profile';
 import 'client/src/styles/form.scss';
 import './profile.scss';
 
 export const PageProfileChangePassword = () => {
+	// const { password } = useSelector('из стора возьму данные пользователя')
+	const [passwords, setPasswords] = useState({
+		oldPassword: 'gfhjkm',
+		newPassword: ''
+	});
 
 	const navigate = useNavigate();
 	const buttonCallback = () => {
-		console.log('Клик');
+		profile.changePassword(passwords);
 	};
+
+	const onInputChange = (value, name) => {
+		setPasswords(_prevValues => ({..._prevValues, [name]: value}));
+	};
+
+	console.log(passwords);
+
 	return (
 		<div className='pageProfile'>
 			<Header>Прифиль: смена пароля</Header>
@@ -20,7 +34,7 @@ export const PageProfileChangePassword = () => {
 					text='назад'
 					className='buttonBack'
 					onClick={() => {
-						navigate('/profile');
+						navigate(-1);
 					}}
 				/>
 
@@ -32,10 +46,14 @@ export const PageProfileChangePassword = () => {
 						label='Текущий пароль'
 						type='password'
 						text='gfhjkm'
+						name='oldPassword'
+						onChange={onInputChange}
 					/>
 					<Input
 						label='Новый пароль'
 						type='password'
+						name='newPassword'
+						onChange={onInputChange}
 					/>
 					<div className='formGroup_btns'>
 						<Button
