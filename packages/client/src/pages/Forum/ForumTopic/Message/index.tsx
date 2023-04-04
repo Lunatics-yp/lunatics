714c458d8901d61fc5ec0e3b03f0b9c59a6c9b94
Moriarty25 <afanasev.andrey99@gmail.com>
2023-04-04 17:02:09 +0300

    refactor: лайк вынесен в отдельный компонент

--- packages/client/src/pages/Forum/ForumTopic/Message/index.tsx
+++ packages/client/src/pages/Forum/ForumTopic/Message/index.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import {Avatar} from 'client/src/components/Avatar';
+import {Like} from 'client/src/components/images/Like';
 import styles from './Message.module.scss';
 import {TMessageProps} from './typing';
 
@@ -20,20 +21,7 @@ export const Message: FC<TMessageProps> = ({message}) => {
 				<p>{text}</p>
 				{/* Чтобы сделать цвет красным нужно добавить класс ${styles.like} */}
 				<div className={`${styles.message__reaction}`}>
-					<svg
-						fill='none'
-						viewBox='0 0 24 24'
-						strokeWidth={1.5}
-						stroke='currentColor'
-					>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935
-							0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1
-							3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
-						/>
-					</svg>
+					<Like/>
 					<span>1</span>
 				</div>
 			</div>
