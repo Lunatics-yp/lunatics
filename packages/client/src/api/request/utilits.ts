1e135bdbb330f1f4b46511d539445244505d345b
yp-ivan <122061506+yp-ivan@users.noreply.github.com>
2023-04-06 19:54:15 +0400

    feat: update packages/client/src/api/request/utilits.ts
    Co-authored-by: Anton Satyshev <satushev@gmail.com>

--- packages/client/src/api/request/utilits.ts
+++ packages/client/src/api/request/utilits.ts
@@ -1,5 +1,5 @@
 import {TErrorAPI} from '../typingAPI';
 
 export const isErrorAPI = (data: unknown | TErrorAPI): data is TErrorAPI => {
-	return (data as TErrorAPI).reason !== undefined;
+	return data && (data as TErrorAPI).reason !== undefined;
 };
