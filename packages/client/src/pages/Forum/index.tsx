1deedc2df3b66c6919fedea505571711035ad332
Moriarty25 <103452841+Moriarty25@users.noreply.github.com>
2023-04-13 12:33:50 +0300

    LUN-63: Добавление ссылки на главную страницу из Форума (#28)
    * feat: добавлена кнопка Домой на странице Форума
    
    * fix: возвращение на главную страницу по клику

--- packages/client/src/pages/Forum/index.tsx
+++ packages/client/src/pages/Forum/index.tsx
@@ -1,10 +1,14 @@
 import {Outlet} from 'react-router-dom';
 import {Header} from 'client/src/components/Header';
+import {Home} from 'client/src/components/images/Home';
 
 export const Forum = () => {
 	return (
 		<>
-			<Header>Forum</Header>
+			<Header>
+				<Home/>
+				<div>Forum</div>
+			</Header>
 			<Outlet/>
 		</>
 	);
