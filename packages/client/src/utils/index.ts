5ae2cc49ab050bfe27572d4b0ac5fae36837e4bc
djpont <alexandervotyakov@gmail.com>
2023-04-21 18:38:19 +0400

    feat: утилита перемешивания массива

---
+++ packages/client/src/utils/index.ts
@@ -0,0 +1,7 @@
+// Функция перемешивания массива. Алгоритм Фишера-Йетса.
+export function shuffleArray<T = unknown>(array: T[]): void {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
