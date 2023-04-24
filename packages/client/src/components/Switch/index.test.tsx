3ca08ebf28a0ea649af3f77385e2d6661d2c665e
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-04-24 14:57:40 +0400

    test: дополнение теста для Switch

--- packages/client/src/components/Switch/index.test.tsx
+++ packages/client/src/components/Switch/index.test.tsx
@@ -3,11 +3,14 @@ import {fireEvent, render, screen} from '@testing-library/react';
 import {Switch} from './index';
 
 let switchValues: string[];
+let lastIndex: number;
 let handleSwitch: jest.Mock;
+
 describe('Switch', () => {
 
 	beforeEach(async () => {
-		switchValues = ['one', 'two', 'three'];
+		switchValues = ['one', 'two', 'three', 'four', 'five'];
+		lastIndex = switchValues.length - 1;
 		handleSwitch = jest.fn();
 	});
 
@@ -19,26 +22,54 @@ describe('Switch', () => {
 		expect(renderedSwitch).toBeInTheDocument();
 	});
 
-	test('should 2 call switch up', () => {
-		const renderedSwitch = render(<Switch list={switchValues} onSwitch={handleSwitch}/>);
+	test('should render with value=`two`', () => {
+		// eslint-disable-next-line max-len
+		const renderedSwitch = render(<Switch list={switchValues} onSwitch={handleSwitch} value={'two'}/>);
 		const renderedValue = renderedSwitch.getByTestId('switch-value');
 
-		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
-		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
-
-		expect(handleSwitch).toBeCalledTimes(2);
-		expect(renderedValue.innerHTML).toBe('three'); // values[2]
+		expect(renderedValue.innerHTML).toBe('two');
 	});
 
-	test('should 2 call switch down', () => {
-		const renderedSwitch = render(<Switch list={switchValues} onSwitch={handleSwitch}/>);
+	test('should call switch up (with loop)', () => {
+		// eslint-disable-next-line max-len
+		const renderedSwitch = render(<Switch list={switchValues} defaultValue={lastIndex-1} onSwitch={handleSwitch}/>);
+		const renderedValue = renderedSwitch.getByTestId('switch-value');
+
+		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
+		expect(renderedValue.innerHTML).toBe(switchValues[lastIndex]);
+
+		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
+		expect(renderedValue.innerHTML).toBe(switchValues[0]);
+
+		expect(handleSwitch).toBeCalledTimes(2);
+	});
+
+	test('should call switch down (with loop)', () => {
+		// eslint-disable-next-line max-len
+		const renderedSwitch = render(<Switch list={switchValues} defaultValue={1} onSwitch={handleSwitch}/>);
 		const renderedValue = renderedSwitch.getByTestId('switch-value');
 
 		fireEvent.click(renderedSwitch.getByTestId('switch-down'));
+		expect(renderedValue.innerHTML).toBe(switchValues[0]);
+
 		fireEvent.click(renderedSwitch.getByTestId('switch-down'));
+		expect(renderedValue.innerHTML).toBe(switchValues[lastIndex]);
 
 		expect(handleSwitch).toBeCalledTimes(2);
-		expect(renderedValue.innerHTML).toBe('two'); // values[1]
+	});
+
+	test('should call switch up (without loop)', () => {
+		// eslint-disable-next-line max-len
+		const renderedSwitch = render(<Switch list={switchValues} defaultValue={lastIndex-1} looped={false} onSwitch={handleSwitch}/>);
+		const renderedValue = renderedSwitch.getByTestId('switch-value');
+
+		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
+		expect(renderedValue.innerHTML).toBe(switchValues[lastIndex]);
+
+		fireEvent.click(renderedSwitch.getByTestId('switch-up'));
+		expect(renderedValue.innerHTML).toBe(switchValues[lastIndex]);
+
+		expect(handleSwitch).toBeCalledTimes(2);
 	});
 
 });
