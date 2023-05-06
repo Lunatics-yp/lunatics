e9b3ec61a5f7f6e63f36fe0e312f74e860221b1c
djpont <alexandervotyakov@gmail.com>
2023-05-07 01:43:27 +0400

    fix: мелкие фиксы

--- packages/client/ssr/ssr.tsx
+++ packages/client/ssr/ssr.tsx
@@ -1,13 +1,10 @@
 import {App} from 'client/src/App';
 import {renderToString} from 'react-dom/server';
-import {StaticRouter} from 'react-router-dom/server';
 import {TSsrRenderProps} from './typing';
 
 export const render: TSsrRenderProps = (url: string, data: unknown) => {
 	console.log('data for redux:', data);
 	return renderToString(
-		<StaticRouter location={url}>
-			<App/>
-		</StaticRouter>,
+		<App/>,
 	);
 };
