a7e662e5794c434a81b7d085ffa4b967a863a87d
Евгения <jenn-m@yandex.ru>
2023-05-09 13:19:25 +0200

    refactor : декларация

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -2,6 +2,8 @@ export {};
 
 declare const __SERVER_PORT__: number;
 
-interface Window {
-	__PRELOADED_STATE__?: Record<string, Record<string, unknown>>;
+declare global {
+	interface Window {
+		__PRELOADED_STATE__?: Record<string, Record<string, unknown>>;
+	}
 }
