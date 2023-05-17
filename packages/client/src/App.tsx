e89c018c2792692eabb29908ca44a4a25d640e7b
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-05-18 01:53:58 +0400

    feat: добавлен роутинг SSR

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,6 +2,7 @@ import {Background} from 'client/src/components/Background';
 import {setupStore} from 'client/src/stores/store';
 import {registerServiceWorker} from 'client/src/utils/serviceWorker';
 import {Provider} from 'react-redux';
+import {SSRRouter} from 'client/src/routers/SSRRouter';
 import 'client/src/styles/errorsPages.scss';
 
 if (process.env.NODE_ENV === 'production') {
@@ -13,7 +14,7 @@ const store = setupStore();
 export const App = () => {
 	return (
 		<Provider store={store}>
-			<h1>SSR PAGE</h1>
+			<SSRRouter/>
 			<Background/>
 		</Provider>
 	);
