2dbad9879581da6471bcdbd581a4a2819763431b
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-05-22 10:33:23 +0400

    fix: исправление App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,6 @@ import {Provider} from 'react-redux';
 import {Background} from 'client/src/components/Background';
 import {setupStore} from 'client/src/stores/store';
 import {registerServiceWorker} from 'client/src/utils/serviceWorker';
-import {Provider} from 'react-redux';
 import {SSRRouter} from 'client/src/routers/SSRRouter';
 import 'client/src/styles/errorsPages.scss';
 
