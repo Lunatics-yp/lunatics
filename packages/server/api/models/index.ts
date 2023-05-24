9b7679ae7eb3feff699ef64ead8a67ccafc8387b
djpont <alexandervotyakov@gmail.com>
2023-05-24 19:31:27 +0400

    feat: вырезал все модели, кроме User (верну в PR другой задачи)

--- packages/server/api/models/index.ts
+++ packages/server/api/models/index.ts
@@ -1,17 +1,5 @@
 import {Users} from './users';
-import {Forums} from './forums';
-import {Topics} from './topics';
-import {Messages} from './messages';
-import {Reactions} from './reactions';
-import {MessagesReactions} from './messagesReactions';
-import {Themes} from './themes';
 
 export {
 	Users,
-	Forums,
-	Topics,
-	Messages,
-	Reactions,
-	MessagesReactions,
-	Themes,
 };
