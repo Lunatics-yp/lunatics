c236dae180f0d879242fa3f471967cb6c62066d6
djpont <alexandervotyakov@gmail.com>
2023-06-22 01:02:08 +0400

    feat: добавил таблицу Leaderboard

---
+++ packages/server/api/models/leaderboard.ts
@@ -0,0 +1,58 @@
+import {DataType, Model} from 'sequelize-typescript';
+import type {ModelAttributes} from 'sequelize/types';
+import {sequelize} from '../sequelize';
+import {Users} from './';
+
+// Модель таблицы Leaderboard
+export type TLeaderboard = {
+	id: number;
+	user_id: number;
+	wins: number;
+	games: number;
+};
+
+const leaderboardModel: ModelAttributes<Model, TLeaderboard> = {
+	id: {
+		type: DataType.INTEGER,
+		primaryKey: true,
+		allowNull: false,
+	},
+	user_id: {
+		type: DataType.INTEGER,
+		allowNull: false,
+		references: {
+			model: Users,
+			key: 'id',
+		},
+		onDelete: 'CASCADE',
+	},
+	wins: {
+		type: DataType.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+	},
+	games: {
+		type: DataType.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+	},
+};
+
+const leaderboardOptions = {
+	timestamps: false,
+	paranoid: false,
+	tableName: 'Leaderboard',
+	indexes: [
+		{
+			unique: true,
+			fields: ['user_id'],
+		},
+	],
+};
+
+const Leaderboard = sequelize.define('Leaderboard', leaderboardModel, leaderboardOptions);
+
+Leaderboard.belongsTo(Users, {foreignKey: 'user_id'});
+Users.hasOne(Leaderboard, {foreignKey: 'user_id'});
+
+export {Leaderboard};
