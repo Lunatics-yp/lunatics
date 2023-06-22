1cb58391f4795f788203587ca8aa41ec6195957b
yp-ivan <122061506+yp-ivan@users.noreply.github.com>
2023-06-22 19:07:14 +0400

    feat: update constants.ts

--- packages/client/src/api/constants.ts
+++ packages/client/src/api/constants.ts
@@ -1,9 +1,1 @@
 export const LEADER_LIMIT_USERS = 10;
-
-export const LEADER_BOARD_URL = 'leaderboard';
-
-export const LEADER_BOARD_BY_TEAM = (teamName: string) => `${LEADER_BOARD_URL}/${teamName}`;
-
-export const LEADER_BOARD_All = `${LEADER_BOARD_URL}/all`;
-
-export const FORUM_URL = '/forum';
