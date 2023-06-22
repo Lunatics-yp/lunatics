68f945337c522d1a9d0ead9d84afb1a3a4034284
djpont <alexandervotyakov@gmail.com>
2023-06-22 17:13:52 +0400

    LUN-109: Локальный лидерборд апи (#84)
    * feat: добавил таблицу Leaderboard
    
    * feat: апи лидерборда, серверая часть
    
    * feat: апи лидерборда, клиентская часть
    
    * feat: кнопка перехода на лидерборд в меню и на главной
    
    Так же добавил иконку Домой на старнице Лидерборда, лидерборд спрятал за авторизацией

--- packages/client/src/api/leaderboard.ts
+++ packages/client/src/api/leaderboard.ts
@@ -1,25 +1,13 @@
-import {api} from './request';
-import {
-	TLeaderboard, TAddLeaderboard, TLeaderboardResponse,
-} from './typing';
-import {
-	TEAM_NAME, LEADER_BOARD_BY_TEAM,
-	LEADER_BOARD_URL,
-	LEADER_BOARD_All,
-} from './constants';
+import {localApi} from 'client/src/api/request/localApi';
+import type {TLeaderboard} from './typing';
 
 export const leaderboardAPI = {
 
 	getAllLeader: (data: TLeaderboard) => (
-		api.post<Array<TLeaderboardResponse>>(LEADER_BOARD_All, data)
+		localApi.post('/leaderboard', {action: 'leaderboard.list', data})
 	),
 
-	addUserToLeaderboard: (data: TAddLeaderboard) => (
-		api.post(LEADER_BOARD_URL, data)
-	),
-
-	getAllLeaderByTeam: (data: TLeaderboard, teamName: string = TEAM_NAME) => (
-		api.post<Array<TLeaderboardResponse>>(LEADER_BOARD_BY_TEAM(teamName), data)
-	),
-
+	addGameResult: (isWinner: boolean) => {
+		localApi.post('/leaderboard', {action: 'leaderboard.set', data: {isWinner}});
+	},
 };
--- packages/server/api/models/index.ts
+++ packages/server/api/models/index.ts
@@ -3,13 +3,15 @@ import {Forums} from './forums';
 import {Topics} from './topics';
 import {Messages} from './messages';
 import {MessagesReactions} from './messagesReactions';
+import {Leaderboard} from './leaderboard';
 
 import type {TUser} from './users';
 import type {TForum} from './forums';
 import type {TTopic} from './topics';
 import type {TMessage} from './messages';
 import type {TMessageReaction} from './messagesReactions';
-import type  {TUserTheme} from 'server/api/theme/typing';
+import type {TUserTheme} from 'server/api/theme/typing';
+import type {TLeaderboard} from './leaderboard';
 
 export {
 	Users,
@@ -17,10 +19,12 @@ export {
 	Topics,
 	Messages,
 	MessagesReactions,
+	Leaderboard,
 	TUser,
 	TForum,
 	TTopic,
 	TMessage,
 	TMessageReaction,
 	TUserTheme,
+	TLeaderboard,
 };
